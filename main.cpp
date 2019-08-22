d255a9a2d06d6b2eb4a72bed2eb69023bf21d8cd
Silvano Cerza <silvanocerza@gmail.com>
2019-08-22 17:58:03 +0200

    Cli arguments are now parsed correctly

--- main.cpp
+++ main.cpp
@@ -1,15 +1,36 @@
 #include <filesystem>
 #include <iostream>
 
+#include <args.hxx>
+
 #include "display.h"
 
 using namespace std;
 namespace fs = std::filesystem;
 
 int main(int argc, const char* argv[]) {
-  // TODO: Takes last argument now, but it must be changed.
-  // Also handle empty file name.
-  Display d(argv[argc - 1]);
+  args::ArgumentParser parser("Snoot, HTTP access log monitor");
+  args::HelpFlag help(parser, "help", "Display this menu", {'h', "help"});
+  args::ValueFlag<fs::path> fileArg(parser, "file", "The HTTP access log file",
+                                    {'f', "file"});
+
+  try {
+    parser.ParseCLI(argc, argv);
+  } catch (const args::Help&) {
+    cout << parser;
+    return 0;
+  } catch (const args::ParseError& e) {
+    cerr << e.what() << endl;
+    cerr << parser << endl;
+    return 1;
+  }
+
+  fs::path file("/tmp/access.log");
+  if (fileArg) {
+    file = fileArg.Get();
+  }
+
+  Display d(file);
   d.run();
   return 0;
 }
