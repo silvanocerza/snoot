f92b3df82619e8696091de46e618a2ee83a0fd0b
Silvano Cerza <silvanocerza@gmail.com>
2019-08-22 14:13:49 +0200

    Implemented Display to show stats

---
+++ src/display.cpp
@@ -0,0 +1,35 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+#include <date.h>
+
+#include "display.h"
+
+using namespace chrono;
+
+Display::Display(const fs::path& logFile) : _monitor(new Monitor(logFile)) {}
+
+[[noreturn]] void Display::run() {
+  _monitor->start();
+
+  while (true) {
+    //    Display::clear();
+
+    auto data = _monitor->logData();
+
+    //    cout << "Requests count in last 10 seconds: " << data.size() << '\n';
+
+    // Shows current date and time.
+    //    auto now = time_point_cast<seconds>(system_clock::now());
+    //    cout << date::format("%F %T", now) << '\n';
+
+    this_thread::sleep_for(1s);
+  }
+}
+
+void Display::clear() noexcept {
+  // It's obviously not the most elegant solution to clear the terminal but it
+  // does the job.
+  cout << string(500, '\n') << endl;
+}
---
+++ src/display.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <filesystem>
+#include <memory>
+
+#include "monitor.h"
+
+using namespace std;
+namespace fs = std::filesystem;
+
+class Display {
+ public:
+  Display(const fs::path& logFile);
+  virtual ~Display() = default;
+
+  Display(Display&&) = delete;
+  Display(const Display&) = delete;
+  Display& operator=(Display&&) = delete;
+  Display& operator=(const Display&) = delete;
+
+  [[noreturn]] void run();
+
+ private:
+  unique_ptr<Monitor> _monitor;
+
+  static void clear() noexcept;
+};
