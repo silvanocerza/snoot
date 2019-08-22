b5a08b436f465b043c5a68133cf7fdf77792f2a3
Silvano Cerza <silvanocerza@gmail.com>
2019-08-22 15:08:42 +0200

    Implemented simple generator of logs

---
+++ generator.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+# Ugly logs generator, most logs won't make much sense
+# Just pipe its output to a file
+
+
+METHODS=("GET" "HEAD" "POST" "PUT" "DELETE" "CONNECT" "OPTIONS" "TRACE")
+RESOURCES=("/report" "/api" "/api/user")
+STATUSES=("200" "202" "400" "404" "500")
+
+# Returns a random element from the passed argument
+# Usage: random_element ARRAY_NAME
+random_element() {
+    local array_name=$1[@]
+    local array=("${!array_name}")
+    echo "${array[((RANDOM % ${#array[*]}))]}"
+}
+
+while [[ 1 ]]
+do
+    host="127.0.0.1"
+    rfc931="-"
+    user=`whoami`
+    now=`date +"%d/%b/%Y:%T %z"`
+    method=`random_element METHODS`
+    resource=`random_element RESOURCES`
+    protocol="HTTP/1.0"
+    status=`random_element STATUSES`
+    size=$(( RANDOM % 200 ))
+
+    echo "$host $rfc931 $user [$now] \"$method $resource $protocol\" $status $size"
+
+    # Sleep for a bit
+    sleep $(echo "$(( $RANDOM % 2 )).$(( $RANDOM % 999 ))")
+
+done
+
