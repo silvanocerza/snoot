f39de301e8cb3e35cc2a9b278b0099b3a8651912
Silvano Cerza <silvanocerza@gmail.com>
2019-08-25 22:27:27 +0200

    Tested Monitor alerts trigger and recovery

--- test/monitor_test.cpp
+++ test/monitor_test.cpp
@@ -15,13 +15,10 @@ TEST_CASE("Monitor") {
   ofstream f(logFile);
   f.close();
 
-  unsigned long hitsThreshold = 10;
-  chrono::seconds alertDuration = 15s;
-
-  SECTION("Verifies Monitor logs correct number of logs") {
+  SECTION("Verifies Monitor logs correct number of log lines") {
     ofstream f(logFile, ios::ate | ios::out);
 
-    Monitor monitor(logFile, hitsThreshold, alertDuration);
+    Monitor monitor(logFile, 10, 15s);
     monitor.start();
 
     auto now = time_point_cast<seconds>(system_clock::now());
@@ -51,6 +48,51 @@ TEST_CASE("Monitor") {
     this_thread::sleep_for(0.5s);
 
     REQUIRE(monitor.logs().size() == 3);
+
+    f.close();
+    fs::remove(logFile);
+  }
+
+  SECTION("Verifies alert trigger when traffic exceeds average") {
+    ofstream f(logFile, ios::ate | ios::out);
+
+    // If there are more than 2 request per second in the last 2 seconds
+    // trigger an alert
+    Monitor monitor(logFile, 2, 2s);
+    monitor.start();
+
+    // Verifies we have no alerts by default
+    REQUIRE(monitor.alerts().size() == 0);
+
+    auto now = time_point_cast<seconds>(system_clock::now());
+    auto dateTime = date::format(locale(""), "%d/%b/%EY:%T %z", now);
+
+    auto logLine = "127.0.0.1 - alien [" + dateTime +
+                   "] \"HEAD /docs/api/tutorial HTTP/1.0\" "
+                   "202 40";
+
+    // Write 5 logs, one over the average to trigger the alert
+    f << logLine << endl;
+    f << logLine << endl;
+    f << logLine << endl;
+    f << logLine << endl;
+    f << logLine << endl;
+
+    // Waits for processing
+    this_thread::sleep_for(0.5s);
+
+    // Verifies new alert is triggered
+    REQUIRE(monitor.alerts().size() == 1);
+    REQUIRE(monitor.alerts().front().hits == 5);
+    REQUIRE(!monitor.alerts().front().hasRecovered());
+
+    // Waits for alert to recover
+    this_thread::sleep_for(3s);
+
+    // Verifies alert has now recovered
+    REQUIRE(monitor.alerts().size() == 1);
+    REQUIRE(monitor.alerts().front().hits == 5);
+    REQUIRE(monitor.alerts().front().hasRecovered());
 
     f.close();
     fs::remove(logFile);
